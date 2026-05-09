9888c928c8c5a32dbb6ffc86ec5bcf80a3ae7868
ahmed-douyry <ahmed.douyry@gmail.com>
2026-05-09 02:17:02 +0100

    feat: Initialize backend project with Spring Boot and JPA
    - Add pom.xml for Maven dependencies and project configuration.
    - Create main application class `ProjetFinalApplication`.
    - Implement DTOs for bank accounts, customers, and account operations.
    - Define entity classes for bank accounts, customers, and account operations.
    - Create repositories for data access.
    - Implement service layer for business logic related to bank accounts and customers.
    - Add controllers for handling HTTP requests related to bank accounts and customers.
    - Configure application properties for MySQL database connection.
    - Add unit tests for application context loading.
    - Initialize frontend subproject.

--- backend/src/main/java/com/example/projet_final/web/BankAccountController.java
+++ backend/src/main/java/com/example/projet_final/web/BankAccountController.java
@@ -9,15 +9,12 @@ import com.example.projet_final.services.BankAccountService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin("*")
 
 public class BankAccountController {
 
--- backend/src/main/java/com/example/projet_final/web/CustmerController.java
+++ backend/src/main/java/com/example/projet_final/web/CustmerController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/custmers")
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class CustmerController {
     private BankAccountService bankAccountService;
     @GetMapping
